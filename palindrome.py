91dfd14429fb435e001a736f5b3012b06061c826
neha1137 <86520366+neha1137@users.noreply.github.com>
2025-10-21 18:16:54 +0530

    Add vowel counting and making Palindrome as a function
    Refactor palindrome check into a function and add vowel counting.

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,22 @@
-while True:
-    str = input("Enter a string: ")
+print("Enter a string to perform the operations of Palindrome and counting vowels : ")
+s = input("Enter a string: ")
+
+def Palindrome(s):
     rev_str = ""
-    for char in str:
+    for char in s:
         rev_str = char + rev_str
-    if str == rev_str:
-        print(f"{str} is a palindrome")
+    if s == rev_str:
+        print(f"{s} is a palindrome")
     else:
-        print(f"{str} is not a palindrome")
+        print(f"{s} is not a palindrome")
+        
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    print(f"The number of vowels in '{s}' is {count}")
+
+Palindrome(s)
+count_vowels(s)
